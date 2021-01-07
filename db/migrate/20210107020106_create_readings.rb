a54c6d8efd87de3c2292f5165953c056ef78a9f2
QueenTesa Fuggett <qfuggett@umich.edu>
2021-01-06 22:36:55 -0500

    update database

--- db/migrate/20210107020106_create_readings.rb
+++ db/migrate/20210107020106_create_readings.rb
@@ -1,7 +1,9 @@
 class CreateReadings < ActiveRecord::Migration[6.0]
   def change
     create_table :readings do |t|
+      t.string :name
       t.string :description
+      t.integer :user_id
 
       t.timestamps
     end
