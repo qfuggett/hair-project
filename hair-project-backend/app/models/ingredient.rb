fa9d4d1ad6820acc410e7c3a2af92381fc61cdad
QueenTesa Fuggett <qfuggett@umich.edu>
2021-01-07 15:48:57 -0500

    add validations to models

--- hair-project-backend/app/models/ingredient.rb
+++ hair-project-backend/app/models/ingredient.rb
@@ -1,2 +1,5 @@
 class Ingredient < ApplicationRecord
+    belongs_to :product
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
--- hair-project-backend/app/models/product.rb
+++ hair-project-backend/app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    has_many :ingredients
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
