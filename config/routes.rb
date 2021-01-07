32e43d4ef5785f9647ce47e3823521abe4342e00
QueenTesa Fuggett <qfuggett@umich.edu>
2021-01-06 22:17:37 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :readings
-  resources :users
+  resources :users do
+    resources :readings
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
