02eadb300ca21ae3b9f9c617b0db2673d957f339
QueenTesa Fuggett <qfuggett@umich.edu>
2021-01-07 19:56:18 -0500

    update js and html

--- hair-project-frontend/index.js
+++ hair-project-frontend/index.js
@@ -4,18 +4,32 @@ window.addEventListener("DOMContentLoaded", () => {
     gotProducts()
 })
 
+//index view
 function getProducts() {
     let main = document.getElementById('main')
-    fetch(BASE_URL + '/products')
+    main.innerHTML = ""
+    fetch(BASE_URL + '/products') //goes to backend to grab products
     .then(res => res.json())
-    .then(products => products.map(product => {
+    .then(products => {
+        products.map(product => {
         console.log(products)
         main.innerHTML += `
-        
+        <li>
+            <a href="a" data-id="${product.id}">${product.name}</a>
+        </li>
         `
-    }).join("")
-)}
+        })
+        clicksToLinks()
+    })
+}
 
-<li>
-    <a href="a" data-id="${product.id}">${product.name}</a>
-</li>
+function clicksToLinks(){
+    const products = document.querySelectorAll("li a")
+    products.forEach(product => {
+        product.addEventListener('click', displayProduct)
+    })
+}
+
+function displayProduct(p){
+    console.log(p.target)
+}
