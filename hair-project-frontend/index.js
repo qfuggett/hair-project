a892ccd65693cd4332a1084aacbbb15da1815824
QueenTesa Fuggett <qfuggett@umich.edu>
2021-01-08 14:01:19 -0500

    update a tag id

--- hair-project-frontend/index.js
+++ hair-project-frontend/index.js
@@ -1,18 +1,43 @@
 const BASE_URL = "http://localhost:3000"
 
 window.addEventListener("DOMContentLoaded", () => {
-    // document.getElementById('product-form').addEventListener('click', displayCreateForm)
+    document.getElementById('show-product-form').addEventListener('click', displayCreateForm)
     document.getElementById('products').addEventListener('click', getProducts)
     getProducts()
 })
 
+function displayCreateForm(){
+    let formDiv = document.querySelector("#product-form")
+    let html = `
+        <form>
+            <label>Name</label>
+            <input type="text" id="name">
+            <input type="submit">
+        </form>
+    `
+    formDiv.innerHTML = html
+    document.querySelector("form").addEventListener(`submit`, createProduct)
+}
+
+function clearForm(){
+    let formDiv = document.querySelector("#product-form")
+    formDiv.innerHTML = ""
+}
+
+function createProduct(e){
+    e.preventDefault()
+
+    console.log(e)
+}
+
+
 //index view
-function getProducts() {
+function getProducts() {            //index
     let main = document.getElementById('main')
     main.innerHTML = ""
-    fetch(BASE_URL + '/products') //goes to backend to grab products
-    .then(res => res.json())
-    .then(products => {
+    fetch(BASE_URL + '/products')   //sends request to return a promise(backend to retrieve products)
+    .then(res => res.json())        //accesses the response and returns it in json format
+    .then(products => {             // promise is resolved and displayed  to the DOM
         products.map(product => {
         console.log(products)
         main.innerHTML += `
@@ -32,17 +57,23 @@ function clicksToLinks(){
     })
 }
 
-function displayProduct(p){
+function displayProduct(p){         //show route
     console.log(p.target)
     let id = p.target.dataset.id
     let main = document.getElementById('main')
     main.innerHTML = ""
-    fetch(BASE_URL + `/products/${id}`)
+    fetch(BASE_URL + `/products/${id}`)         
     .then(resp => resp.json())
     .then(todo => {
         main.innerHTML = `
-        <h3>${todo.name}</h3>
+        <h3>${product.name}</h3>
+        
+        <a href="a" data-id="${product.id}">${product.name}</a>
+        ${ingredient.name}
         `
-        // product.ingredients.forEach
+        product.ingredients.forEach(ingredient => {     
+            console.log(ingredient.name);
+        })
     })
 }
+
