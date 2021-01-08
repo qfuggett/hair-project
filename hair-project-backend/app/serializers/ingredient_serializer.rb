d41afd9ff547aacc35b847d0d1208dfcd235a1bf
QueenTesa Fuggett <qfuggett@umich.edu>
2021-01-07 20:56:04 -0500

    add id attributes to serializers

--- hair-project-backend/app/serializers/ingredient_serializer.rb
+++ hair-project-backend/app/serializers/ingredient_serializer.rb
@@ -1,3 +1,3 @@
 class IngredientSerializer < ActiveModel::Serializer
-    attributes :description
+    attributes :id, :name, :description
 end
--- hair-project-backend/app/serializers/product_serializer.rb
+++ hair-project-backend/app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-    attributes :name
+    attributes :id, :name
 end
